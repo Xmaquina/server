d8431aa0857b92e56c926ecc3cc514a6ef01ae13
Gustavo Coelho <gutorc@hotmail.com>
2017-04-04 00:18:37 -0300

    Begin of server
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ client.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3.4
+import socket
+import sys
+
+MSGLEN = 20
+
+class MySocket:
+    """demonstration class only
+      - coded for clarity, not efficiency
+    """
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            print("Passou aqui")
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+def main():
+    print("Call function") 
+    m = MySocket()
+    m.connect("localhost", 8888)
+    text = m.myreceive()
+    print(text)
+
+if __name__ == "__main__":
+    print("Starting")
+    main()
